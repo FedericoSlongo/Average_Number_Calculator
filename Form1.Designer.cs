402e3029d110ccab95822d29c3ed63f0eff8c7ac
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2022-03-21 19:14:52 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Testing_Info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txb = new System.Windows.Forms.TextBox();
+            this.risult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calcolatore di media:";
+            // 
+            // txb
+            // 
+            this.txb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txb.Location = new System.Drawing.Point(16, 32);
+            this.txb.Name = "txb";
+            this.txb.Size = new System.Drawing.Size(100, 26);
+            this.txb.TabIndex = 1;
+            this.txb.TextChanged += new System.EventHandler(this.txb_TextChanged);
+            // 
+            // risult
+            // 
+            this.risult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.risult.AutoSize = true;
+            this.risult.Location = new System.Drawing.Point(12, 61);
+            this.risult.Name = "risult";
+            this.risult.Size = new System.Drawing.Size(80, 20);
+            this.risult.TabIndex = 2;
+            this.risult.Text = "Risultato: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 87);
+            this.Controls.Add(this.risult);
+            this.Controls.Add(this.txb);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Calc Media";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txb;
+        private System.Windows.Forms.Label risult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testing_Info
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txb_TextChanged(object sender, EventArgs e)
+        {
+            double temp = 0, ris = 0, cont = 0;
+            string[] inp = txb.Text.Split(' ');
+            for (int i = 0; i < inp.Length; i++)
+            {
+                if (double.TryParse(inp[i], out temp))
+                {
+                    double.TryParse(inp[i], out temp);
+                    ris += temp;
+                    cont++;
+                }else if(inp[i] == "")
+                {
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Errore il valore di input non è valido","Errore",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            ris = ris / cont;
+            risult.Text = $"Risultato : {ris}";
+        }
+    }
+}
